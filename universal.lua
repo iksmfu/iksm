980e7671d85f2a59684018ff24224855bbfee6e1
s-o-a-b <144854842+s-o-a-b@users.noreply.github.com>
2023-09-24 13:43:30 -0700

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,73 +1,12 @@
-if getgenv().nexus then return end 
-
 local HttpService = game:GetService("HttpService")
-local fileName = "FLORENCE/SETTINGS/" .. game.PlaceId .. '.txt'
-getgenv().settings = {}
-getgenv().nexus = true
 
-if isfile("FLORENCE/SETTINGS/" .. game.PlaceId .. '.txt') then
-    local sl, er = pcall(function()
-        getgenv().settings = game:GetService('HttpService'):JSONDecode(readfile("FLORENCE/SETTINGS/" .. game.PlaceId .. '.txt'))
-    end)
-    if er ~= nil then
-        forceServerHop()
-        return
-    end
-end 
+local Fluent = loadstring(game:HttpGet("https://github.com/s-o-a-b/nexus/releases/download/nexus/nexus.txt"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/s-o-a-b/nexus/main/assets/SaveManager"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/s-o-a-b/nexus/main/assets/InterfaceManager"))()
 
-writefile("FLORENCE/SETTINGS/" .. game.PlaceId .. '.txt', HttpService:JSONEncode(getgenv().settings))
+local Options = Fluent.Options
+SaveManager:SetLibrary(Fluent)
 
-function forceServerHop()
-    local Api = "https://games.roblox.com/v1/games/"
-    local placeId, jobId = game.PlaceId, game.JobId
-    local serversUrl = Api .. placeId .. "/servers/Public?sortOrder=Desc&limit=100"
-
-    local function ListServers(cursor)
-        local raw = game:HttpGet(serversUrl .. (cursor and "&cursor=" .. cursor or ""))
-        return HttpService:JSONDecode(raw)
-    end
-
-    local nextCursor
-    repeat
-        local serversData = ListServers(nextCursor)
-        for _, server in ipairs(serversData.data) do
-            if server.playing < server.maxPlayers and server.id ~= jobId then
-                local success, result = pcall(TeleportService.TeleportToPlaceInstance, TeleportService, placeId, server.id, LocalPlayer)
-                if success then
-                    break
-                end
-            end
-        end
-        nextCursor = serversData.nextPageCursor
-    until not nextCursor
-end
-
-local myEvent = Instance.new("BindableEvent")
-local connection = myEvent.Event:Connect(function()
-end)
-
-local function createLoop(callback)
-    return spawn(function()
-        while task.wait() do
-            if connection.Connected == true then
-                local success, result = pcall(function() 
-                    callback()
-                end)
-            end
-        end
-    end)
-end
-
-createLoop(function()
-    for key, value in pairs(getgenv().settings) do
-	if connection.Connected == true and getgenv().settings.AutoSave == true then 
-        getgenv().settings[key] = value
-        writefile(fileName, HttpService:JSONEncode(getgenv().settings))
-		end
-    end
-end)
-
-local Fluent = loadstring(game:HttpGet("https://github.com/13B8B/nexus/releases/download/nexus/nexus.txt"))()
 --[[
    premium = true
 ]]
@@ -101,29 +40,6 @@ Tabs.Main:AddParagraph({
     Title = "Nexus Universal",
     Content = "Welcome to Nexus Universal!\n\nWe're excited to introduce the upcoming Universal script to enhance your experience. Currently in development, this script is designed to bring you a host of new features and improvements.\n\nOur dedicated team is working diligently to make this script a reality. While it's not ready just yet, we're making great progress.\n\nStay tuned for updates, and thank you for your patience and support! https://discord.gg/KHKu84ZyhD"
 })
-
-local KeyBindName = getgenv().settings.KeyBind or ""
-
-local Keybind = Tabs.Settings:AddKeybind("Keybind", {
-    Title = "KeyBind",
-    Mode = "Toggle",
-    Default = KeyBindName,
-    ChangedCallback = function(New)
-        KeyBindName = New.Name
-        getgenv().settings.KeyBind = New.Name  
-    end
-})
-
-game:GetService("UserInputService").InputBegan:Connect(function(input, gameProcessedEvent)
-    local settingsKeyBind = getgenv().settings.KeyBind
-
-    if input.KeyCode == Enum.KeyCode.Home or settingsKeyBind == input.KeyCode.Name then
-        if game:GetService("CoreGui").ScreenGui.Frame.Visible then
-            Fluent:Notify({Title = 'Window Minimized', Content = 'Press ' .. settingsKeyBind .. ' to Open the UI', Duration = 5 })
-        end
-        Window:Minimize() 
-    end
-end)
 
 local Toggle = Tabs.Settings:AddToggle("Toggle", {
     Title = "Auto Save Settings",
@@ -201,131 +117,17 @@ Tabs.Settings:AddButton({
     end
 })
 
-createLoop(function()
-    if premium == "premium" then
-        game.Players:Chat("nexus-premium")
-    else task.wait(math.random(0.1, 1)) 
-        game.Players:Chat("nexus-is-back")
-    end
-end)
+-- Set libraries and folders
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+SaveManager:SetIgnoreIndexes({})
+SaveManager:IgnoreThemeSettings()
+InterfaceManager:SetFolder("FLORENCE")
+SaveManager:SetFolder("FLORENCE")
 
-----------// PREMIUM \\----------
-Tab = premium == "premium" and Tabs.Premium:AddButton({
-    Title = "Kick",
-    Callback = function()
-        game.Players:Chat(".k " .. getgenv().Selected)
-    end 
-})
-Tab = premium == "premium" and Tabs.Premium:AddButton({
-    Title = "Kill",
-    Callback = function()
-        game.Players:Chat(".r " .. getgenv().Selected)
-    end
-})
-Tab = premium == "premium" and Tabs.Premium:AddButton({
-    Title = "Teleport",
-    Callback = function()
-        game.Players:Chat(".b " .. getgenv().Selected)
-    end
-})
-Tab = premium == "premium" and Tabs.Premium:AddButton({
-    Title = "Shut Game Down",
-    Callback = function()
-        game.Players:Chat(".s " .. getgenv().Selected)
-    end
-})
-Tab = premium == "premium" and Tabs.Premium:AddButton({
-    Title = "Freeze",
-    Callback = function()
-        game.Players:Chat(".f " .. getgenv().Selected)
-    end
-})
-Tab = premium == "premium" and Tabs.Premium:AddButton({
-    Title = "Unfreeze",
-    Callback = function()
-        game.Players:Chat(".u " .. getgenv().Selected)
-    end
-})
+-- Build interface section and load the game
+InterfaceManager:BuildInterfaceSection(Tabs.Settings, Tabs.Premium)
+SaveManager:Load(game.PlaceId)
 
-task.spawn(function()
-    while task.wait() do 
-        local playersService = game:GetService("Players")
-        local textChatService = game:GetService("TextChatService")
-        local lplr = playersService.LocalPlayer
-        local localPlayerNameWithoutUnderscores = lplr.Name:gsub("_", "")
-        
-        for _, player in ipairs(playersService:GetPlayers()) do
-            player.Chatted:Connect(function(msg)
-                local success, errorMessage = pcall(function()
-                    local processedMsg = msg:gsub("_", "")
-                    if processedMsg == ".k " .. localPlayerNameWithoutUnderscores then
-                        game.Players.LocalPlayer:kick("nexus-premium user has kicked you")
-                    elseif processedMsg == ".r " .. localPlayerNameWithoutUnderscores then
-                        game.Players.LocalPlayer.Character.Humanoid.Health = 0
-                    elseif processedMsg == ".b " .. localPlayerNameWithoutUnderscores then
-                        game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart").CFrame = workspace[player.Name].HumanoidRootPart.CFrame
-                    elseif processedMsg == ".s " .. localPlayerNameWithoutUnderscores then
-                        game:Shutdown()
-                    elseif processedMsg == ".f " .. localPlayerNameWithoutUnderscores then
-                        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
-                    elseif processedMsg == ".u " .. localPlayerNameWithoutUnderscores then
-                        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false    
-                    end  
-                end)
-            end) 
-        end
-        wait(1)  
-    end 
-end)
-local nexus = {}
-local updatedPlayers = {} 
-local Dropdown
-
-local function UpdateDropdownValues()
-    if Dropdown then
-        Dropdown:SetValues(nexus)
-    end
-end
-local function RemovePlayer(player)
-    for i, playerName in ipairs(nexus) do
-        if playerName == player.Name then
-            table.remove(nexus, i)
-            updatedPlayers[player] = nil
-            UpdateDropdownValues()
-            break
-        end
-    end
-end
-game.Players.PlayerRemoving:Connect(function(player)
-    RemovePlayer(player)
-end)
-
-task.spawn(function()
-    while wait() do 
-        for _, player in ipairs(game.Players:GetPlayers()) do
-            player.Chatted:Connect(function(msg)
-                if msg == "nexus-is-back" and not updatedPlayers[player] then
-                    if not table.find(nexus, player.Name) and player ~= game.Players.LocalPlayer then
-                        local playerNameWithoutUnderscores = player.Name:gsub("_", "")
-                        table.insert(nexus, playerNameWithoutUnderscores)
-                        print("Detected:", playerNameWithoutUnderscores)
-                        updatedPlayers[player] = true  
-                        UpdateDropdownValues() 
-                    end
-                end  
-            end) 
-        end
-    end
-end)
-
-Dropdown = premium == "premium" and Tabs.Premium:AddDropdown("Dropdown", {
-    Title = "Select Nexus User",
-    Values = nexus, 
-    Multi = false,
-    Default = "",
-    Callback = function(value)
-        getgenv().Selected = value
-    end
-})
-
+-- Select the first tab in the window
 Window:SelectTab(1)
