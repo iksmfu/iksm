7cafe4b6f40a5434ec8d946a4cb4995306d2b594
s-o-a-b <144854842+s-o-a-b@users.noreply.github.com>
2023-09-29 14:31:29 -0700

    Update 13772394625.lua

--- modules/13772394625.lua
+++ modules/13772394625.lua
@@ -113,7 +113,7 @@ local function checkBalls()
             game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position, child.Position)
 
 			local distance = LocalPlayer:DistanceFromCharacter(child.Position)
-			if distance <= 46 then
+			if distance <= 45 then
 				--game:GetService("ReplicatedStorage").Remotes.ParryButtonPress:Fire()
                 game:GetService("VirtualInputManager"):SendKeyEvent(true, "F", false, nil)
                 game:GetService("VirtualInputManager"):SendKeyEvent(false, "F", false, nil)
